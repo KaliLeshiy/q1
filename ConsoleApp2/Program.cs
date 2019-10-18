00e848a7a3400fff4e185aef07766870eec4f9d1
Алексей Калинин <Kalinin.98221@outlook.com>
2019-10-18 12:30:46 +0500

    qwe

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double t = -1.4;
+            double tKon = 4.5;
+            double h = 0.3;
+            double y = 0;
+            while (t <= tKon)
+            {
+                if (t <= 3)
+                    y = Math.Sin(4 * t);
+                else
+                    y = Math.Cos(t - 3 / t);
+                Console.WriteLine("t = {0:0.##}\ty= {1:0.###}", t, y);
+                t += h;
+            }
+            Console.ReadLine();
+
+
+
+        }
+            
+    }
+}
